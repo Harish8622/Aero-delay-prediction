30a8b259e2eae1f407890862d24bcdcf6bd3bbf7
= <=>
2025-08-09 20:27:57 +0100

    feat: created pre processing pipeline

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Minimal ML pipeline to predict flight delays.
 
 ## Quickstart
 pip install -r requirements.txt
+
+to run pre processing
+
+run
+
+python ml_pipelines/run_preprocessing.py
---
+++ ml_pipelines/preprocessing/run_preprocessing.py
@@ -0,0 +1,33 @@
+# ml_pipelines/preprocessing/run_preprocessing.py
+import subprocess, sys, argparse
+from ml_pipelines.preprocessing.params import PreprocessingParams
+
+
+def main():
+    p = PreprocessingParams()
+
+    parser = argparse.ArgumentParser(description="Run preprocessing")
+    parser.add_argument("--input_flights_table", default=p.input_flights_table)
+    parser.add_argument("--input_airport_table", default=p.input_airport_table)
+    parser.add_argument("--output_path", "--out", default=p.output_path)
+    parser.add_argument("--cache_dir", default=p.cache_dir)
+    args = parser.parse_args()
+
+    cmd = [
+        sys.executable,
+        "-m",
+        "src.preprocessing",  # run as module so 'from src...' works
+        "--input_flights_table",
+        args.input_flights_table,
+        "--input_airport_table",
+        args.input_airport_table,
+        "--output_path",
+        args.output_path,
+        "--cache_dir",
+        args.cache_dir,
+    ]
+    subprocess.run(cmd, check=True)
+
+
+if __name__ == "__main__":
+    main()
